e1491f9f02cb2ff4ebd962167920b9103af811b3
VladosSid <sidorsv121097@gmail.com>
2022-08-30 21:22:02 +0300

    task 01 completed

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,4 +1,32 @@
-import { galleryItems } from './gallery-items.js';
-// Change code below this line
+import { galleryItems } from "./gallery-items.js";
 
-console.log(galleryItems);
+const boxEl = document.querySelector(".gallery");
+
+const newEl = galleryItems
+  .map(
+    (el) => `<div class="gallery__item">
+  <a class="gallery__link" href="${el.original}" target="_parent">
+    <img
+      class="gallery__image"
+      src="${el.preview}"
+      data-source="${el.original}"
+      alt="${el.description}"
+    />
+</a>
+</div>`
+  )
+  .join("");
+
+boxEl.innerHTML = newEl;
+
+boxEl.addEventListener("click", (event) => {
+  event.preventDefault();
+  const showEl = basicLightbox.create(`
+    <img src="${event.target.dataset.source}">
+`);
+  showEl.show();
+
+  document.addEventListener("keyup", () => {
+    showEl.close();
+  });
+});
