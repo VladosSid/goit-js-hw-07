29a570fc31d4a4979f9b306ed9652ae9519a4878
VladosSid <sidorsv121097@gmail.com>
2022-09-02 20:27:24 +0300

    07 - 2 completed

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,23 @@
-import { galleryItems } from './gallery-items.js';
+import { galleryItems } from "./gallery-items.js";
 // Change code below this line
 
 console.log(galleryItems);
+
+const listEl = document.querySelector(".gallery");
+
+const newEl = galleryItems
+  .map(
+    (el) =>
+      `<a class="gallery__item" href="${el.original}">
+    <img class="gallery__image" src="${el.preview}" alt="${el.description}" />
+  </a>`
+  )
+  .join("");
+
+listEl.innerHTML = newEl;
+
+const gallery = new SimpleLightbox(".gallery a");
+gallery.on("show.simplelightbox", function (el) {
+  gallery.options.captionsData = "alt";
+  gallery.options.captionDelay = "250ms";
+});
